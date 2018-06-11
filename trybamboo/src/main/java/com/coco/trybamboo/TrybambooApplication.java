da7a91202d37d96a613bb338967a4b6cd1d466fc
coco <pinkannacoco@gmail.com>
2018-06-11 12:00:56 +0800

    First commit

---
+++ trybamboo/src/main/java/com/coco/trybamboo/TrybambooApplication.java
@@ -0,0 +1,12 @@
+package com.coco.trybamboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrybambooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrybambooApplication.class, args);
+	}
+}
---
+++ trybamboo/src/test/java/com/coco/trybamboo/TrybambooApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coco.trybamboo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrybambooApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
